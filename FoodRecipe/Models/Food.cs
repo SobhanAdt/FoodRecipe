5f851114fa43d76651c2a06314c8c31a67c1a779
Karanous <mhmdiali810@gmail.com>
2021-01-15 11:33:43 +0330

    FoodModel

---
+++ FoodRecipe/Models/Food.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipe.Models
+{
+    public class Food
+    {
+        public string food { get; set; }
+        public string foodThumb { get; set; }
+        public string id { get; set; }
+    }
+}
---
+++ FoodRecipe/Models/FoodByIngredientThemealdb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipe.Models
+{
+    public class FoodByIngredientThemealdb
+    {
+        public string strMeal { get; set; }
+        public string strMealThumb { get; set; }
+        public string idMeal { get; set; }
+    }
+}
