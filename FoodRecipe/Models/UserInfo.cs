e9587c1962f2d9a94af2d35f833f54bb9be19578
Your Name <you@example.com>
2021-01-15 10:22:34 +0330

    1

---
+++ FoodRecipe/Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipe.Models
+{
+    public class UserInfo
+    {
+        public string email { get; set; }
+
+        public List<favorites> favorites { get; set; }
+    }
+}
---
+++ FoodRecipe/Models/favorites.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace FoodRecipe.Models
+{
+    public class favorites
+    {
+        public  String nameFood { get; set; }
+    }
+}
