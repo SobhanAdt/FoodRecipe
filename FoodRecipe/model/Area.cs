6bbf4323acc0db858be4f66f68a40ebf92b83fe0
shadi_rahbar74@yahoo.com <shadi_rahbar74@yahoo.com>
2021-01-15 10:55:00 +0330

    page 2

---
+++ FoodRecipe/model/Area.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipe.model
+{
+    public class Area
+    {
+        public string strArea { get; set; }
+    }
+}
