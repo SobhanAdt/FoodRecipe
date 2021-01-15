abdf4183d105f5074f228516b441678f648ff898
shadi_rahbar74@yahoo.com <shadi_rahbar74@yahoo.com>
2021-01-15 11:08:39 +0330

    add models categori and ingredient

---
+++ FoodRecipe/Models/categories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipe.Models
+{
+    public class categories
+    {
+        public string strCaregory { get; set; }
+    }
+}
---
+++ FoodRecipe/Models/ingredients.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipe.Models
+{
+    public class ingredients
+    {
+        public string id { get; set; }
+        public string ingredient { get; set; }
+    }
+}
